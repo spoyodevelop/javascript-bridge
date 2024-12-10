4856b1aa43569bdf0464156197b00d84cf190713
spoyodevelop <kjy9505@gmail.com>
2024-12-10 14:12:35 +0900

    docs: 게임플레이 로직 기능목록 재작성

--- src/MainController.js
+++ src/MainController.js
@@ -3,11 +3,8 @@ import InputView from './InputView.js';
 export default class MainController {
   async startProgram() {
     // const bridgeLength = await InputView.getBridgeLengthFromUser();
-
     // const movement = await InputView.getMovementFromUser();
-
-    const retry = await InputView.getRetryFromUser();
-
-    console.log(retry);
+    // const retry = await InputView.getRetryFromUser();
+    // console.log(retry);
   }
 }
